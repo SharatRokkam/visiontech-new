4c77af694f253b77fa4529ebaa1c7b7293fa0cf5
SharatRokkam <rokkamsharath1@gmail.com>
2026-07-27 20:02:17 +0530

    js variable and intro

---
+++ Javascript/basics.js
@@ -0,0 +1,46 @@
+// Printing 
+// console.log("In the file")
+
+// alert("Hey! I am learning Javascript")
+
+// variables is like container or box where you can store any value to use it later. 
+// three ways to create variable - var, let, const
+
+// var myage = prompt("enter your age");
+// let myname= prompt("enter your name")
+// const mySalary = prompt("enter you salary");
+// console.log(myage)
+// console.log(myname)
+// console.log(mySalary)
+
+// Rules to create variable 
+
+// var _myname = "Pranathi";  //correct
+// let myname = "Prasanth"; //correct
+
+// const myname123 = "Sharath"; //incorrect
+
+// // const total score = "45" //incorrect
+// let no_of_employees = 440; //correct
+
+// let hello;
+
+// let HELLO;
+
+
+// Camelcase convention to create the variable 
+
+// let myEmailId = "test@gmail.com"
+// let mySalary;
+// const myPanCard;
+// var myAadhar;
+
+// snakecase convention 
+// let my_email_id;
+// let no_of_employees; 
+
+
+var myFavoriteFestival = "Diwali";
+console.log(myFavoriteFestival);
+
+
