8227cdd0755bd60fe84c9b3e8a0161a5e0143305
SharatRokkam <rokkamsharath1@gmail.com>
2026-07-29 19:46:06 +0530

    operators

---
+++ Javascript/operators.js
@@ -0,0 +1,66 @@
+// javascript, variables, datatypes 
+
+// Operators 
+// are used to perform operations on the operand
+// console.log(5 + 3) // 5 , 3 operand --> + is operator
+// console.log(5 - 3)
+// console.log(5 * 3)
+
+// Types of operators in  Javascipt 
+
+// 1. Arithematic Operator - +, -,*, /, %
+// console.log(5 + 3) // 5 , 3 operand --> + is operator
+// console.log(5 - 3)
+// console.log(5 * 3)
+// console.log(5 / 3)
+// console.log(5 % 3)
+
+// 2. Assignment Operator - =, +=, -=, *=, /=, %=
+let marks = 10;
+
+// marks = marks + 1;
+marks += 1; //shorthand
+
+// console.log(marks -=3);
+// console.log(marks *=4); //44 
+// console.log(marks /=4); //11
+// console.log(marks %=4); //3
+// marks /=2;  //2
+// marks %= 2; //0
+// console.log(marks)
+
+
+//3. Comparison Operator - >, <, >=, <=, !=, ==, ===
+// console.log(2 == "2") // double equal will check the value
+// console.log(2 === "2") // triple equals will check value then data type
+
+// value == value  //true
+// number === string //false
+
+// console.log(10 > 4); // true
+// console.log(10 < 4); //false
+// console.log(10 >= 4); //true
+// console.log(10 <= 4); //false
+// console.log(10 != 10); //true
+
+
+// 4. Logical Operator
+// let age = 18;
+// console.log( age > 18 && age < 20)  //both condition satifies then true
+
+// console.log(!(age == 18));
+
+// !(true) -> false
+// !(False) => true
+
+// console.log("Hello!" + "Guys")
+
+// // concat 
+// console.log("Hello".concat('World'))
+
+// console.log(typeof("3"));
+// console.log(typeof("number"));
+
+
+
+
