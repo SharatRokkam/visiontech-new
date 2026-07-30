a29ac838a06f46daf6393fa07b19918a74e086a4
SharatRokkam <rokkamsharath1@gmail.com>
2026-07-30 19:55:02 +0530

    conditions

---
+++ Javascript/conditions.js
@@ -0,0 +1,83 @@
+// Condition are the situation that allows any person to take decision
+
+// Morning - > keeping sleeping or wake up
+// go out - > take right or left
+
+// teach the computer to take decision , as it is a dumb machine
+
+// condition ------ Javascript
+
+// if, if else, if else if
+// let myAge = 18;
+
+// if (myAge > 18) {
+//   console.log("you can drive");
+// } else if (myAge === 18) {
+//   console.log("you can also drive");
+// } else {
+//   console.log("you can't drive");
+// }
+
+// let marks = 69;
+
+// if (marks >= 90) {
+//   console.log("you get A+");
+// } else if (marks >= 80) {
+//   console.log("you get B");
+// } else if (marks >= 70) {
+//   console.log("you get C");
+// } else if (marks >= 60) {
+//   console.log("you get D");
+// } else {
+//   console.log("you failed");
+// }
+
+// ternary operator
+
+// let age = 18;
+
+// // condition ? "true" : "false "
+// result = age >= 18 ? "you can drive" : "you can't drive"
+// console.log(result);
+
+// let maxScore = 90;
+
+// console.log(maxScore === "90" ? "true" : "false")
+
+// == vs ===
+// == - check value
+// === -> value then data type
+
+// Switch Statement in JS - Home work
+
+// Looping ----to repeat something until a condition becomes false
+// listen music - loop
+
+// console.log()
+
+// for, while, do while(programming) / in only  JS- foreach, forof, for in
+
+
+// increment and decrement
+// let a = 10;
+// a++; //10+1 -> 11
+// a--; //11-1 -> 10
+// a++  //10 + 1 -> 11
+// a-- //11 -1 => 10
+// a-- //10 - 1 => 9
+
+// console.log(a); //
+
+// for(initialization, condition, increment/decrement)
+
+// for(let i = 2;  i <= 10; i++){
+//   console.log(i)
+// }
+
+// let i = 1 ; 1 <= 10 => 1 =>1 + 1 = 2
+// let i = 2; 2 <= 10 => 2 => 2 + 1 = > 3
+// let i = 3; 3<=10 => 3 => 3+ 1 => 4
+// let i = 4 ; 4 <= 10 => 4 => 4 + 1 => 5
+// ...
+// let i = 10 ; 10 <= 10 => 10 => 10 + 1 => 11
+// let i = 11; 11 <= 10 = false 
